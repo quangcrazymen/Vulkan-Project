36bab2c18fd77c52bedbc1207770e7c1ceed85a2
Quang <quangcrazymen@gmail.com>
2022-09-02 11:26:28 +0700

    Add: check validation layer

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -4,56 +4,109 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-class HelloTriangleApplication{
-    public:
-        void run(){
-            initWindow();
-            initVulkan();
-            mainLoop();
-            cleanup();
-        }
-    private:
-        GLFWwindow* window;
-        void initWindow(){
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
 
-            glfwInit();
-            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-            window = glfwCreateWindow(WIDTH,HEIGHT,"Vulkan",nullptr,nullptr);
-            
-        }
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
 
-        void initVulkan(){
-            
-        }
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
 
-        void mainLoop(){
-            while(!glfwWindowShouldClose(window)){
-                glfwPollEvents();
+private:
+    GLFWwindow *window;
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+        std::vector<int> arrayTest = {2, 4, 6, 4};
+        for (int x : arrayTest)
+            std::cout << x << " ";
+        createInstance();
+        
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    bool checkValidationLayerSupport()
+    {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName:validationLayers){
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0){
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound){
+                return false;
             }
         }
+        return true;
+    }
 
-        void cleanup(){
-            glfwDestroyWindow(window);
-            glfwTerminate();
+    void createInstance(){
+        if (enableValidationLayers && !checkValidationLayerSupport()){
+            throw std::runtime_error("validation layers requested, but now available!");
         }
-
-
+    }
 };
 
-int main(){
+int main()
+{
     HelloTriangleApplication app;
 
-    try {
+    try
+    {
         app.run();
-    }catch (const std::exception& e){
-        std::cerr<<e.what()<<std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
-    
     }
     return EXIT_SUCCESS;
 }
